d612d995c8c2af2092ac429fdd79f79ca33ef94e
Jossl123 <jossl1.gau@gmail.com>
2023-09-26 15:55:40 +0200

    add multi hand detection

--- cam_test.py
+++ cam_test.py
@@ -9,8 +9,9 @@ class VideoAnalyser:
         self.mp_drawing = mp.solutions.drawing_utils
         self.webcam = cv2.VideoCapture(0)
         self.frame_nb = 0
-        self.lml=[]
-        self.lml_px=[]
+        self.hands_lml=[]
+        self.h = 0
+        self.w = 0
     def get_img(self):
         # n_size = (int(img.shape[1] * 0.6), int(img.shape[0] * 0.6))
         # img = cv2.resize(img, n_size, interpolation=cv2.INTER_AREA)
@@ -20,18 +21,20 @@ class VideoAnalyser:
         results = self.mp_hands_p.process(img)
         #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         return results
+    def to_pix(self, p):
+        xc, yc = int(p[0] * self.w), int(p[1] * self.h)
+        return [xc, yc]
     def update(self):
-        success,img = self.get_img()
+        success, img = self.get_img()
+        self.h, self.w, _ = img.shape
         results = self.analyse_img(img)
         if self.frame_nb%1 == 0:
             if results.multi_hand_landmarks:
-                self.lml = []
-                self.lml_px = []
-                for id, lm in enumerate(results.multi_hand_landmarks[0].landmark):
-                    h, w, _ = img.shape
-                    xc, yc = int(lm.x * w), int(lm.y * h)
-                    self.lml.append([lm.x, lm.y, lm.z])
-                    self.lml_px.append([xc, yc])
+                self.hands_lml = []
+                for i in range(len(results.multi_hand_landmarks)):
+                    self.hands_lml.append([])
+                    for id, lm in enumerate(results.multi_hand_landmarks[i].landmark):
+                        self.hands_lml[i].append([lm.x, lm.y, lm.z])
                 self.draw_marks(img, results.multi_hand_landmarks)
                 self.draw_index_thumb_dist(img)
 
@@ -39,14 +42,17 @@ class VideoAnalyser:
         self.frame_nb = (self.frame_nb + 1)%100
 
     def draw_index_thumb_dist(self, img):
-        if len(self.lml_px)< 9:return
-        x1, y1 = self.lml_px[4][0], self.lml_px[4][1]
-        x2, y2 = self.lml_px[8][0], self.lml_px[8][1]
-        cv2.circle(img, (x1, y1), 10, (255, 0, 128), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 10, (255, 0, 128), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 128), 3)
-        distance = math.hypot(x2 - x1, y2 - y1)
-        cv2.putText(img, str(int(distance)), (x1+30, y1+20), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 128), 3)
+        for hand in self.hands_lml:
+            if len(hand)< 9:return
+            x1, y1 = hand[4][0], hand[4][1]
+            x2, y2 = hand[8][0], hand[8][1]
+            x1, y1 = self.to_pix((x1, y1))
+            x2, y2 = self.to_pix((x2, y2))
+            cv2.circle(img, (x1, y1), 10, (255, 0, 128), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 128), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 128), 3)
+            distance = math.hypot(x2 - x1, y2 - y1)
+            cv2.putText(img, str(int(distance)), (x1+30, y1+20), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 128), 3)
 
     def draw_marks(self, img , multi_hand_marks):
         for hand_landmarks in multi_hand_marks:
